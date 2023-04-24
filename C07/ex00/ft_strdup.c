014cca44cc7884f672781e6e436cf4a4f68be44b
Mehmet Ali <86076087+malidegirmenci@users.noreply.github.com>
2023-04-24 22:39:24 +0300

    Add files via upload
    c07 project added

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdegirme <mdegirme@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 01:39:16 by mdegirme          #+#    #+#             */
+/*   Updated: 2023/04/14 01:53:20 by mdegirme         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	copy = malloc((sizeof(char) * i) + 1);
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdegirme <mdegirme@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 01:41:23 by mdegirme          #+#    #+#             */
+/*   Updated: 2023/04/14 04:08:43 by mdegirme         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	len;
+	int	i;
+	int	*arr;
+
+	if (min >= max)
+		return (0);
+	len = max - min;
+	arr = (int *)malloc(len * sizeof(int));
+	if (!arr)
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		arr[i] = min;
+		min++;
+		i++;
+	}
+	return (arr);
+}
